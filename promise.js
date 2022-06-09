22178eb83f2c8f196b3d908a4fd650f95f3f53ef
nurulannisyah <ayu23nurul@gmail.com>
2022-06-09 21:31:32 +0700

    Tugas Promise menghitung reaksi nonton film

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaction) => {
+  const result = await Promise.all([
+    promiseTheaterIXX(),
+    promiseTheaterVGC()
+  ])
+
+  return result.reduce(
+    (totalReaction, theater) => totalReaction + theater.filter(({ hasil }) => hasil === reaction).length, 0
+  )
+};
 
 module.exports = {
   promiseOutput,
